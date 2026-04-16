a3cc6e8d9d4f3b9a0f16803f904c9f0ca5df9bfb
 <vitor.206alexandrino@gmail.com>
2026-04-16 20:06:20 -0300

    cenario e iluminacao feitas

--- player.gd
+++ player.gd
@@ -1,22 +1,22 @@
 extends CharacterBody3D
 
 
-var speed: int = 4
-var rotation_speed: int = 2
+@export var speed: int = 4
+@export var rotation_speed: int = 2
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
 
 func _physics_process(delta: float) -> void:
 	
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("cam_left"):
 		rotation.y += rotation_speed * delta
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("cam_right"):
 		rotation.y -= rotation_speed * delta
 	
 	var input_dir = 0
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("move_back"):
 		input_dir += 1
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("move_front"):
 		input_dir -= 1
 		
 	var direction = (transform.basis * Vector3(0, 0, input_dir)).normalized()
